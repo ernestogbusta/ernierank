f3fa136c1eb3bf03b288146167d00fd60bb5e6ae
Ernesto García Bustamante <ernestogarciabustamante@ordenador-portatil-de-ernesto.home>
2024-03-27 00:39:24 +0100

    Segunda iteración subiendo de nivel

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from sitemap_crawler import SitemapExtractor
 from content_extractor import SEOContentAnalyzer
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/scrape', methods=['POST'])
 def scrape_site():
