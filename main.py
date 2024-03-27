95db632284be1eaaeab358eff1e41c7dcbcb4071
Ernesto García Bustamante <ernestogarciabustamante@Ordenador-portatil-de-Ernesto.local>
2024-03-27 16:44:31 +0100

    Segunda iteración

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import nltk
+nltk.download('punkt')
 from sitemap_crawler import SitemapExtractor
 from content_extractor import SEOContentAnalyzer
 
